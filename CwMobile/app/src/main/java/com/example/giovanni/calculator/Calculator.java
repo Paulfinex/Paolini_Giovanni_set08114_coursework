b2b9ef27b42129e652dd775ee88d8b6a75c99ad1
Paulfinex <sentius96@gmail.com>
2018-03-14 07:55:23 +0000

    Add files via upload

--- CwMobile/app/src/main/java/com/example/giovanni/calculator/Calculator.java
+++ CwMobile/app/src/main/java/com/example/giovanni/calculator/Calculator.java
@@ -13,15 +13,11 @@ import java.text.DecimalFormat;
 public class Calculator extends AppCompatActivity {
 
     private Boolean newOperation=true;
-    private static final char ADD = '+';
-    private static final char SUB = '-';
-    private static final char MUL = '*';
-    private static final char DIV = '/';
     DecimalFormat decimal = new DecimalFormat("#.#######"); // Limit the digits of a decimal number
-    private char LastChange;  //Keeps track of the last action executed
+    private String lastChange;  //Keeps track of the last action executed
     private double firstNum=Double.NaN;  //First number of the operation
-    private double secondNum; //Second number of the operation
-
+    private double secondNum=Double.NaN; //Second number of the operation
+    private double ans=Double.NaN;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -168,7 +164,7 @@ public class Calculator extends AppCompatActivity {
                 String temp = editText.getText().toString();
                 if(!temp.contains("."))
                 {
-                    if (isEmpty(editText)) {
+                    if (isEmptyEt(editText)) {
                         editText.setText("0.");
                     } else
                         editText.append(".");
@@ -188,16 +184,30 @@ public class Calculator extends AppCompatActivity {
             }
         });
 
+        // Button ANS
+        final Button button_ans = findViewById(R.id.button_ans);
+        button_ans.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                EditText editText = findViewById(R.id.editText);
+                if(!Double.isNaN(ans))
+                {
+                    editText.setText(ans+"");
+                }
+
+            }
+        });
+
         // Button delete
         final Button button_delete = findViewById(R.id.button_delete);
         button_delete.setOnClickListener(new View.OnClickListener()
         {
             public void onClick(View v)
             {
-                NewOperation();
                 EditText editText = findViewById(R.id.editText);
                 String text = editText.getText().toString();
-                if(!isEmpty(editText))
+                if(!isEmptyEt(editText))
                 editText.setText(text.substring(0, text.length() - 1));
 
             }
@@ -209,16 +219,27 @@ public class Calculator extends AppCompatActivity {
         {
             public void onClick(View v)
             {
-                NewOperation();
                 EditText editText = findViewById(R.id.editText);
                 TextView textView= findViewById(R.id.textView);
-                String text = editText.getText().toString() + textView.getText().toString() ;
-                if(!text.contains("+")&&!text.contains("-")&&!text.contains("x")&&!text.contains("÷")&&!isEmpty(editText))
-                {
-                    firstNum = Double.parseDouble(text);
-                    textView.setText(firstNum+"+");
-                    editText.setText("");
-                }
+                String text = editText.getText().toString() ;
+                    if(newOperation && !isEmptyEt(editText))
+                    {
+                        firstNum = Double.parseDouble(text);
+                        textView.setText(firstNum+"+");
+                        editText.setText("");
+                        newOperation=false;
+                    }
+                    else if(!isEmptyEt(editText))
+                    {
+                        multipleInput();
+                        textView.setText(textView.getText().toString()+"+");
+                    }
+                if(isEmptyEt(editText) && !isEmptyTw(textView) && !newOperation)
+                    {
+                        text = textView.getText().toString();
+                        textView.setText(text.substring(0, text.length() - 1) + "+");
+                    }
+                lastChange="+";
             }
         });
 
@@ -228,16 +249,28 @@ public class Calculator extends AppCompatActivity {
         {
             public void onClick(View v)
             {
-                NewOperation();
                 EditText editText = findViewById(R.id.editText);
                 TextView textView= findViewById(R.id.textView);
-                String text = editText.getText().toString() + textView.getText().toString() ;
-                if(!text.contains("+")&&!text.contains("-")&&!text.contains("x")&&!text.contains("÷")&&!isEmpty(editText))
+                String text = editText.getText().toString() ;
+                if(newOperation && !isEmptyEt(editText))
                 {
                     firstNum = Double.parseDouble(text);
                     textView.setText(firstNum+"-");
                     editText.setText("");
+                    newOperation=false;
                 }
+                else if(!isEmptyEt(editText))
+                {
+                    multipleInput();
+                    textView.setText(textView.getText().toString()+"-");
+                }
+                if(isEmptyEt(editText) && !isEmptyTw(textView)&& !newOperation)
+                {
+                    text = textView.getText().toString();
+                    textView.setText(text.substring(0, text.length() - 1) + "-");
+                }
+
+                lastChange="-";
             }
         });
 
@@ -247,16 +280,27 @@ public class Calculator extends AppCompatActivity {
         {
             public void onClick(View v)
             {
-                NewOperation();
                 EditText editText = findViewById(R.id.editText);
                 TextView textView= findViewById(R.id.textView);
-                String text = editText.getText().toString() + textView.getText().toString() ;
-                if(!text.contains("+")&&!text.contains("-")&&!text.contains("x")&&!text.contains("÷")&&!isEmpty(editText))
+                String text = editText.getText().toString() ;
+                if(newOperation && !isEmptyEt(editText))
                 {
                     firstNum = Double.parseDouble(text);
                     textView.setText(firstNum+"x");
                     editText.setText("");
+                    newOperation=false;
                 }
+                else if(!isEmptyEt(editText))
+                {
+                    multipleInput();
+                    textView.setText(textView.getText().toString()+"x");
+                }
+                if(isEmptyEt(editText) && !isEmptyTw(textView)&& !newOperation)
+                {
+                    text = textView.getText().toString();
+                    textView.setText(text.substring(0, text.length() - 1) + "x");
+                }
+                lastChange="x";
             }
         });
 
@@ -266,18 +310,111 @@ public class Calculator extends AppCompatActivity {
         {
             public void onClick(View v)
             {
-                NewOperation();
                 EditText editText = findViewById(R.id.editText);
                 TextView textView= findViewById(R.id.textView);
-                String text = editText.getText().toString() + textView.getText().toString() ;
-                if(!text.contains("+")&&!text.contains("-")&&!text.contains("x")&&!text.contains("÷")&&!isEmpty(editText))
+                String text = editText.getText().toString() ;
+                if(newOperation && !isEmptyEt(editText))
                 {
                     firstNum = Double.parseDouble(text);
                     textView.setText(firstNum+"÷");
                     editText.setText("");
+                    newOperation=false;
                 }
+                else if(!isEmptyEt(editText))
+                {
+                    multipleInput();
+                    textView.setText(textView.getText().toString()+"÷");
+                }
+                if(isEmptyEt(editText) && !isEmptyTw(textView)&& !newOperation)
+                {
+                    text = textView.getText().toString();
+                    textView.setText(text.substring(0, text.length() - 1) + "÷");
+                }
+                lastChange="÷";
             }
         });
+
+        // Button sqrt
+        final Button button_sqrt = findViewById(R.id.button_sqrt);
+        button_sqrt.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                EditText editText = findViewById(R.id.editText);
+                TextView textView= findViewById(R.id.textView);
+                String text = editText.getText().toString() ;
+                if(!isEmptyEt(editText))
+                {
+                    ans=Math.sqrt(Double.parseDouble(text));
+                    textView.setText(String.valueOf(decimal.format(ans))+"");
+                    editText.setText("");
+                    newOperation=true;
+                }
+
+            }
+        });
+
+        // Button sin
+        final Button button_sin = findViewById(R.id.button_sin);
+        button_sin.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                EditText editText = findViewById(R.id.editText);
+                TextView textView= findViewById(R.id.textView);
+                String text = editText.getText().toString() ;
+                if(!isEmptyEt(editText))
+                {
+                    ans=Math.sin(Double.parseDouble(text));
+                    textView.setText(String.valueOf(decimal.format(ans))+"");
+                    editText.setText("");
+                    newOperation=true;
+                }
+
+            }
+        });
+
+        // Button cos
+        final Button button_cos = findViewById(R.id.button_cos);
+        button_cos.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                EditText editText = findViewById(R.id.editText);
+                TextView textView= findViewById(R.id.textView);
+                String text = editText.getText().toString() ;
+                if(!isEmptyEt(editText))
+                {
+                    ans=Math.cos(Double.parseDouble(text));
+                    textView.setText(String.valueOf(decimal.format(ans))+"");
+                    editText.setText("");
+                    newOperation=true;
+                }
+
+            }
+        });
+
+        // Button tan
+        final Button button_tan = findViewById(R.id.button_tan);
+        button_tan.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                EditText editText = findViewById(R.id.editText);
+                TextView textView= findViewById(R.id.textView);
+                String text = editText.getText().toString() ;
+                if(!isEmptyEt(editText))
+                {
+                    ans=Math.tan(Double.parseDouble(text));
+                    textView.setText(String.valueOf(decimal.format(ans))+"");
+                    editText.setText("");
+                    newOperation=true;
+                }
+
+            }
+        });
+
+
 
         // Button result
         final Button button_result = findViewById(R.id.button_result);
@@ -287,7 +424,8 @@ public class Calculator extends AppCompatActivity {
             {
                 EditText editText = findViewById(R.id.editText);
                 TextView textView= findViewById(R.id.textView);
-                if(!isEmpty(editText) && firstNum!=Double.NaN)
+                lastChange="";
+                if(!isEmptyEt(editText) && firstNum!=Double.NaN)
                 {
                     secondNum = Double.parseDouble(editText.getText().toString());
                     String text = textView.getText().toString();
@@ -301,18 +439,21 @@ public class Calculator extends AppCompatActivity {
                                 double num=firstNum+secondNum;
                                 textView.setText(String.valueOf(decimal.format(num)));
                                 editText.setText("");
+                                ans=num;
                                 newOperation=true;
                                 break;
                             case "-":
                                 num=firstNum-secondNum;
                                 textView.setText(String.valueOf(decimal.format(num)));
                                 editText.setText("");
+                                ans=num;
                                 newOperation=true;
                                 break;
                             case "x":
                                 num=firstNum*secondNum;
                                 textView.setText(String.valueOf(decimal.format(num)));
                                 editText.setText("");
+                                ans=num;
                                 newOperation=true;
                                 break;
                             case "÷":
@@ -320,10 +461,13 @@ public class Calculator extends AppCompatActivity {
                                 {
                                     num = firstNum / secondNum;
                                     textView.setText(String.valueOf(decimal.format(num)));
+                                    editText.setText("");
+                                    ans=num;
                                 }
                                 else
                                 {
                                     textView.setText("Math Error");
+                                    newOperation = true;
                                 }
                                 editText.setText("");
                                 newOperation=true;
@@ -342,10 +486,14 @@ public class Calculator extends AppCompatActivity {
 
     }
 
-    private boolean isEmpty(EditText etText) {
+    private boolean isEmptyEt(EditText etText) {
         if (etText.getText().toString().trim().length() > 0)
             return false;
-
+        return true;
+    }
+    private boolean isEmptyTw(TextView twText) {
+        if (twText.getText().toString().trim().length() > 0)
+            return false;
         return true;
     }
     private void NewOperation()
@@ -353,12 +501,62 @@ public class Calculator extends AppCompatActivity {
         EditText editText = findViewById(R.id.editText);
         TextView textView= findViewById(R.id.textView);
         if(newOperation) {
-            editText.setText("");
+            lastChange="";
             textView.setText("");
             firstNum=Double.NaN;
             secondNum=Double.NaN;
-            newOperation=false;
         }
+    }
+
+    private void multipleInput()
+    {
+        EditText editText = findViewById(R.id.editText);
+        TextView textView= findViewById(R.id.textView);
+        double num = 0;
+        if(lastChange!="")
+        {
+            secondNum =Double.parseDouble( editText.getText().toString());
+        }
+        if(secondNum!=Double.NaN)
+        {
+            switch(lastChange) {
+                case "+":
+                    num=firstNum+secondNum;
+                    firstNum=num;
+                    secondNum=Double.NaN;
+                    textView.setText(String.valueOf(decimal.format(num)));
+                    break;
+                case "-":
+                    num=firstNum-secondNum;
+                    firstNum=num;
+                    secondNum=Double.NaN;
+                    textView.setText(String.valueOf(decimal.format(num)));
+                    break;
+                case "x":
+                    num=firstNum*secondNum;
+                    firstNum=num;
+                    secondNum=Double.NaN;
+                    textView.setText(String.valueOf(decimal.format(num)));
+                    break;
+                case "÷":
+                    if(firstNum!=0)
+                    {
+                        num=firstNum/secondNum;
+                        firstNum=num;
+                        secondNum=Double.NaN;
+                        textView.setText(String.valueOf(decimal.format(num)));
+                    }
+                    else
+                    {
+                        textView.setText("Math Error");
+                        newOperation = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+        editText.setText("");
     }
 }
 
